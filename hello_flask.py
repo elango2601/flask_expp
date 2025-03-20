0c7f55e528d5d94287517edb61559cf4c135b87a
Elango <elangopugal26@mail.com>
2025-03-20 15:04:55 +0530

    Redirect the page

--- hello_flask.py
+++ hello_flask.py
@@ -10,6 +10,14 @@ def index():
 def template():
     return render_template("index.html")
     
+@app.route("/pageone")
+def pageone():
+    return render_template("firstpage.html")
+ 
+@app.route("/pagetwo")
+def pagetwo():
+    return render_template("secondpage.html")
+    
 if __name__=="__main__":
     app.run(port=1245)
     
