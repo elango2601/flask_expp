6fafdd9950243e00e8afa4d7f7439fa73e0a84c7
Elango <elangopugal26@mail.com>
2025-03-20 15:19:31 +0530

    Version 2.0

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import datetime
 
 app=Flask(__name__)
 
@@ -17,6 +18,21 @@ def pageone():
 @app.route("/pagetwo")
 def pagetwo():
     return render_template("secondpage.html")
+    
+@app.route("/frontpage")
+def frontpage():
+    return render_template("frontpage.html")
+    
+@app.route("/resultpage")
+def resultpage():
+    return render_template("resultpage.html")
+    
+@app.route("/printtime")
+def printtime():
+    print()
+    print(datetime.datetime.now())
+    print()
+    return redirect("/resultpage")
     
 if __name__=="__main__":
     app.run(port=1245)
